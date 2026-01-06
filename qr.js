0af92727fe6d1f69269a10a471577897b169ebd8
MAFIA ADEEL <adeelsabqi11@gmail.com>
2026-01-06 22:35:15 +0500

    Update qr.js

--- qr.js
+++ qr.js
@@ -56,13 +56,13 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'ARSLAN-MD~' + b64data });
+				   let session = await Qr_Code_By_Mbuvi_Tech.sendMessage(Qr_Code_By_Mbuvi_Tech.user.id, { text: 'MAFIA-MD~' + b64data });
 	
 				   let MBUVI_MD_TEXT = `
 ╔════════════════════◇
 ║『 SESSION CONNECTED』
-║ ✨Arslan-MD🔷
-║ ✨ArslanMD OFFICIAL🔷
+║ ✨MAFIA-MD🔷
+║ ✨MAFIA OFFICIAL🔷
 ╚════════════════════╝
 
 
